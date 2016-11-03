0d594647dc3e03a1adb68e3657eca732b05bd1d6
Donovan Graham <donovan@platform7.com>
2016-11-03 22:25:17 +0200

    fix: wrap up post benchmark

--- Readme.md
+++ Readme.md
@@ -1,7 +1,18 @@
+# Setup
+Build the docker image and install the necessary dependencies
+```sh
 docker-compose build --pull --no-cache --force-rm graph
-docker-compose run graph yarn install
-docker-compose up graph
+docker-compose run graph npm install
+```
 
+# Starting the service (development mode)
+Use docker compose to start the service on port 3000
+```sh
+$ docker-compose up graph
+```
 
-ab -n 1000 -c 2 http://localhost:3000/canvas
-ab -n 1000 -c 2 http://localhost:3000/svg
+# Benchmark the service (production mode)
+```sh
+$ docker-compose up graph-prod
+$ ab -l -k -H "Accept-Encoding: gzip, deflate" -p $(pwd)/src/debugData.json -T application/json -c 4 -n 1000 http://localhost:3000/chart
+```
--- src/app.js
+++ src/app.js
@@ -1,89 +1,90 @@
-const express = require('express');
+const cluster = require('cluster');
+const os = require('os')
 const fs = require('fs');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const d3 = require('d3');
 require('d3-selection-multi');
 
 const jsdom = require("jsdom");
-
 const Canvas = require('canvas');
 
 const app = express();
-
-const parseTime = d3.timeParse("%d-%b-%y");
-const data = JSON.parse(fs.readFileSync('./data.json', 'utf8')).map(d => ({
-  date: parseTime(d.date),
-  close: d.close,
-}));
-
-
-
-
-
-
-const xAxis = (context, x, height) => {
-  const tickCount = 10;
-  const tickSize = 6;
-  const ticks = x.ticks(tickCount);
-  const tickFormat = x.tickFormat();
-
-  context.beginPath();
-  ticks.forEach(d =>  {
-    context.moveTo(x(d), height);
-    context.lineTo(x(d), height + tickSize);
-  });
-  context.strokeStyle = "black";
-  context.stroke();
-
-  context.textAlign = "center";
-  context.textBaseline = "top";
-  ticks.forEach(d => context.fillText(tickFormat(d), x(d), height + tickSize));
-};
-
-const yAxis = (context, y, height) => {
-  const tickCount = 10;
-  const tickSize = 6;
-  const tickPadding = 3;
-  const ticks = y.ticks(tickCount);
-  const tickFormat = y.tickFormat(tickCount);
-
-  context.beginPath();
-  ticks.forEach(d => {
-    context.moveTo(0, y(d));
-    context.lineTo(-6, y(d));
-  });
-  context.strokeStyle = "black";
-  context.stroke();
-
-  context.beginPath();
-  context.moveTo(-tickSize, 0);
-  context.lineTo(0.5, 0);
-  context.lineTo(0.5, height);
-  context.lineTo(-tickSize, height);
-  context.strokeStyle = "black";
-  context.stroke();
-
-  context.textAlign = "right";
-  context.textBaseline = "middle";
-  ticks.forEach(d => context.fillText(tickFormat(d), -tickSize - tickPadding, y(d)));
-
-  context.save();
-  context.rotate(-Math.PI / 2);
-  context.textAlign = "right";
-  context.textBaseline = "top";
-  context.font = "bold 10px sans-serif";
-  context.fillText("Price (US$)", -10, 10);
-  context.restore();
-};
-
+app.use(bodyParser.json({ limit: '1mb' }));
+// app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 
 app.get('/canvas', (req, res) => {
+  const parseTime = d3.timeParse("%d-%b-%y");
+  const data = JSON.parse(fs.readFileSync('./data.json', 'utf8')).map(d => ({
+    date: parseTime(d.date),
+    close: d.close,
+  }));
+
   const canvas = new Canvas(800, 800);
   const context = canvas.getContext('2d');
 
   const margin = { top: 20, right: 20, bottom: 30, left: 50 };
   const width = canvas.width - margin.left - margin.right;
   const height = canvas.height - margin.top - margin.bottom;
+
+
+  const xAxis = (context, x, height) => {
+    const tickCount = 10;
+    const tickSize = 6;
+    const ticks = x.ticks(tickCount);
+    const tickFormat = x.tickFormat();
+
+    context.beginPath();
+    ticks.forEach(d =>  {
+      context.moveTo(x(d), height);
+      context.lineTo(x(d), height + tickSize);
+    });
+    context.strokeStyle = "black";
+    context.stroke();
+
+    context.textAlign = "center";
+    context.textBaseline = "top";
+    ticks.forEach(d => context.fillText(tickFormat(d), x(d), height + tickSize));
+  };
+
+  const yAxis = (context, y, height) => {
+    const tickCount = 10;
+    const tickSize = 6;
+    const tickPadding = 3;
+    const ticks = y.ticks(tickCount);
+    const tickFormat = y.tickFormat(tickCount);
+
+    context.beginPath();
+    ticks.forEach(d => {
+      context.moveTo(0, y(d));
+      context.lineTo(-6, y(d));
+    });
+    context.strokeStyle = "black";
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(-tickSize, 0);
+    context.lineTo(0.5, 0);
+    context.lineTo(0.5, height);
+    context.lineTo(-tickSize, height);
+    context.strokeStyle = "black";
+    context.stroke();
+
+    context.textAlign = "right";
+    context.textBaseline = "middle";
+    ticks.forEach(d => context.fillText(tickFormat(d), -tickSize - tickPadding, y(d)));
+
+    context.save();
+    context.rotate(-Math.PI / 2);
+    context.textAlign = "right";
+    context.textBaseline = "top";
+    context.font = "bold 10px sans-serif";
+    context.fillText("Price (US$)", -10, 10);
+    context.restore();
+  };
 
   const x = d3.scaleTime()
       .range([0, width]);
@@ -115,6 +116,11 @@ app.get('/canvas', (req, res) => {
 
 
 app.get('/svg', (req, res) => {
+  const parseTime = d3.timeParse("%d-%b-%y");
+  const data = JSON.parse(fs.readFileSync('./data.json', 'utf8')).map(d => ({
+    date: parseTime(d.date),
+    close: d.close,
+  }));
 
   const width = 800;
   const height = 800;
@@ -146,11 +152,6 @@ app.get('/svg', (req, res) => {
 
   const y = d3.scaleLinear()
       .rangeRound([graphHeight, 0]);
-
-  // const line = d3.line()
-  //     .x(d => x(d.date))
-  //     .y(d => y(d.close))
-  //     .curve(d3.curveStep);
 
   var area = d3.area()
       .curve(d3.curveStep)
@@ -185,40 +186,35 @@ app.get('/svg', (req, res) => {
       fill: 'url(#stripes)',
     })
     .attr("d", area);
-    // .attr("style", "fill: lightblue; stroke: steelblue; stroke-width: 0.5px;stroke-left:none;")
-    // .attr("d", line);
+
 
   res.set({ 'Content-Type': 'image/svg+xml' });
   res.send(document.getElementById('svg').outerHTML);
 });
 
 
+app.post('/test', (req, res) => {
+  console.log(req.body[0]);
+  res.send('OK');
+});
 
 
-
-app.get('/chart', (req, res) => {
-
-  // const parseTime = d3.timeParse("%d-%b-%y");
+app.post('/chart', (req, res) => {
   const parseTime = d3.timeParse("%Y-%m-%d");
 
-  const chartData = JSON.parse(fs.readFileSync('./debugData.json', 'utf8')).map(d => ({
+  // const chartData = JSON.parse(fs.readFileSync('./debugData.json', 'utf8')).map(d => ({
+  const chartData = req.body.map(d => ({
     date: parseTime(d.date),
-    savings: d.savings,
-    regularNominal: d.regularNominal,
-    regularReal: d.regularReal,
-    additionalNominal: d.additionalNominal,
-    additionalReal: d.additionalReal,
+    saving: d.savings,
+    regular: d.regularNominal,
+    additional: d.additionalNominal,
   }));
 
-  // const maxY = d3.max([
-  //   d3.max(_chartData, d => d.savings),
-  //   d3.max(_chartData, d => d.regularNominal),
-  //   d3.max(_chartData, d => d.additionalNominal),
-  //   shortfallAmountAbsolute,
-  // ]);
-
-  //
-  // const zeroedData = area(Array(chartData.length).fill(0));
+  const maxY = d3.max([
+    d3.max(chartData, d => d.saving),
+    d3.max(chartData, d => d.regular),
+    d3.max(chartData, d => d.additional),
+  ]);
 
   const width = 800;
   const height = 800;
@@ -229,21 +225,27 @@ app.get('/chart', (req, res) => {
   const tmpl = `
     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="svg" width="${width}" height="${height}">
       <style>
-        .grid .tick {
+        .tick {
           stroke-opacity: 0.1;
           shape-rendering: crispEdges;
         }
       </style>
       <g transform="translate(${margin.left},${margin.top})">
-          <g class="axis axis--x"></g>
-          <g class="axis axis--y"></g>
-          <g class="grid grid--x"></g>
-          <g class="grid grid--y"></g>
-          <path class="line line--area"></path>
+          <g class="axis axis__x"></g>
+          <g class="axis axis__y"></g>
+          <path class="line area__savings"></path>
+          <path class="line area__regular"></path>
+          <path class="line area__additional"></path>
       </g>
       <defs>
-        <pattern id="stripes" width="3" height="3" patternUnits="userSpaceOnUse" patternTransform="rotate(45 0 0)">
+        <pattern id="stripe__red" width="3" height="3" patternUnits="userSpaceOnUse" patternTransform="rotate(45 0 0)">
           <line stroke="red" strokeWidth="1px" y2="3"/>
+        </pattern>
+        <pattern id="stripe__green" width="3" height="3" patternUnits="userSpaceOnUse" patternTransform="rotate(45 0 0)">
+          <line stroke="green" strokeWidth="1px" y2="3"/>
+        </pattern>
+        <pattern id="stripe__blue" width="3" height="3" patternUnits="userSpaceOnUse" patternTransform="rotate(45 0 0)">
+          <line stroke="blue" strokeWidth="1px" y2="3"/>
         </pattern>
       </defs>
     </svg>`;
@@ -253,108 +255,69 @@ app.get('/chart', (req, res) => {
   const svg = d3.select(document.getElementById('svg'));
   const g = svg.select('g');
 
-  var x = d3.scaleTime()
-      // .domain([new Date(2016, 08, 1), new Date(2083, 08, 2)])
+  const x = d3.scaleTime()
       .domain(d3.extent(chartData, d => d.date))
       .range([0, graphWidth]);
 
-  // const x = d3.scaleTime()
-  //     .ticks(d3.time.years, 1)
-  //     // .domain([new Date(2000, 0, 1), new Date(2000, 0, 2)])
-  //     .rangeRound([0, graphWidth]);
-  //
-
   const y = d3.scaleLinear()
-      .rangeRound([graphHeight, 0]);
+      .rangeRound([graphHeight, 0])
+      .domain([0, maxY])
+      .nice();
 
-  // const line = d3.line()
-  //     .x(d => x(d.date))
-  //     .y(d => y(d.close))
-  //     .curve(d3.curveStep);
-
-
-  // x.domain(d3.extent(chartData, d => d.date));
-  y.domain(d3.extent(chartData, d => d.regularNominal));
-
-
-  const xTicks = d3.timeYear.every(10);
   const xAxis = d3.axisBottom(x)
-    .ticks(xTicks)
-
-  const xGrid = d3.axisBottom(x)
-    .ticks(xTicks)
+    .ticks(d3.timeYear.every(10))
     .tickSize(-graphHeight)
-    .tickFormat("");
+    .tickPadding(10);
 
-  g.select('g.axis--x')
+  g.select('g.axis__x')
     .attrs({ transform: `translate(0,${graphHeight})` })
     .call(xAxis);
 
-  g.select('g.grid--x')
-    .attrs({ transform: `translate(0,${graphHeight})` })
-    .call(xGrid)
-
-  const yTicks = 5;
   const yAxis = d3.axisLeft(y)
-    .ticks(yTicks);
-
-  const yGrid = d3.axisLeft(y)
-    .ticks(yTicks)
+    .ticks(5)
     .tickSize(-graphWidth)
-    .tickFormat("");
+    .tickPadding(10);
 
-  g.select('g.axis--y')
-    .call(yAxis)
-    .append("text")
-      .attrs({
-        fill: "#000",
-        transform: "rotate(-90)",
-        y: 10,
-        x: -10,
-        dy: "0.71em",
-        style: "text-anchor:end",
-      })
-      .text("Price ($)");
+  g.select('g.axis__y')
+    .call(yAxis);
 
-
-  g.select('g.grid--y')
-    .attrs({ transform: `translate(0,0)` })
-    .call(yGrid)
-
-
-
-  const area = d3.area()
+  const savingsArea = d3.area()
       .curve(d3.curveStep)
       .x(d => x(d.date))
       .y0(graphHeight)
-      .y1(d => y(d.regularNominal));
+      .y1(d => y(d.saving));
+
+  const regularArea = d3.area()
+      .curve(d3.curveStep)
+      .x(d => x(d.date))
+      .y0(graphHeight)
+      .y1(d => y(d.regular));
+
+  const additionalArea = d3.area()
+      .curve(d3.curveStep)
+      .x(d => x(d.date))
+      .y0(graphHeight)
+      .y1(d => y(d.additional));
 
 
-  // const line = d3.line()
-  //     .x(d => x(d.date))
-  //     .y(d => y(d.regularNominal))
-  //     .curve(d3.curveStep);
-
-
-  g.select('path.line--area')
+  g.select('path.area__savings')
     .datum(chartData)
-    .attrs({
-      fill: 'url(#stripes)',
-    })
-    .attr("d", area);
-    //
-    // .attr("style", "fill: lightblue; stroke: steelblue; stroke-width: 0.5px;stroke-left:none;")
-    // .attr("d", line);
+    .attr('fill', 'url(#stripe__red)')
+    .attr("d", savingsArea);
 
-  // const savingsData = chartData.map(x => x.savings);
-  // const regularData = chartData.map(x => x.regularNominal);
-  // const additionalData = chartData.map(x => x.additionalNominal);
+  g.select('path.area__regular')
+    .datum(chartData)
+    .attr('fill', 'url(#stripe__green)')
+    .attr("d", regularArea);
 
+  g.select('path.area__additional')
+    .datum(chartData)
+    .attr('fill', 'url(#stripe__blue)')
+    .attr("d", additionalArea);
 
-
-  // res.set({ 'Content-Type': 'image/svg+xml' });
-  // res.send(document.getElementById('svg').outerHTML);
-  res.send(document.documentElement.outerHTML);
+  res.set({ 'Content-Type': 'image/svg+xml' });
+  res.send(document.getElementById('svg').outerHTML);
+  // res.send(document.documentElement.outerHTML);
 });
 
 
@@ -391,4 +354,15 @@ app.get('/cavas-direct', (req, res) => {
 
 app.get('/', (req, res) => res.send('Up and running'));
 
-app.listen(3000, () => console.log("App listening on port 3000"));
+
+if (process.env.NODE_ENV === 'production' && cluster.isMaster) {
+  const numCPUs = os.cpus().length;
+  let i = 0;
+  for (i; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker) =>
+    console.log(`worker ${worker.process.pid} died`));
+} else {
+  app.listen(3000, () => console.log('App listening on port 3000'));
+}
